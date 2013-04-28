706fc19cd0c037f6d4d1476a3d9824806373c55d
xuxintao <physicxborn@163.com>
2013-04-28 10:26:46 +0800

    comments for 'PrintPDF.py'

--- Frontend/PrintPDF.py
+++ Frontend/PrintPDF.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 2013-3-28
 
@@ -26,16 +27,17 @@ class PrintTable():
             a = [['BoxID','IgnitionTime','FieldID','Name']]
         else:
             return
-
+#根据表名来设置各字段名
         with session.begin():
             data = session.query(ScriptData).all()
-
+#取出ScriptData里的所有数据
         for row1 in data:
             with session.begin():
                 row2 = session.query(IgnitorsData).filter_by(UUID = row1.IgnitorID).first()
+#取出IgnitorsData里的第一行数据
             with sess.begin():
                 row3 = sess.query(FireworksData).filter_by(UUID = row1.FireworkID).first()
-
+#取出FireworksData里的第一行数据
             b = []
 
             if self.table == TABLEFields:
@@ -54,16 +56,16 @@ class PrintTable():
                 b.append(row1.IgnitionTime)
                 b.append(row3.Name)
                 b.append(row2.FieldID)
-
+#根据表名将相应字段内的数据写进数组里
             a.append(b)
-
+#将数组b的内容放进二维数组a里
         self.creatTable(a)
-
+#调用函数creatTable
     def Pages(self,canvas,doc):
         canvas.saveState()
         canvas.drawString((doc.pagesize[0]/2)-5,25,u'%d'%(doc.page))
         canvas.restoreState()
-    
+#生成页脚页码的函数  
     def creatTable(self, data):
         elements = []
         styles = getSampleStyleSheet()
@@ -71,13 +73,17 @@ class PrintTable():
 
         if self.table == TABLEFields:
             doc = SimpleDocTemplate(os.path.join (appdata, 'pdf', 'Fields.pdf'), pagesize = pagesizes.A4)
+            #设置文件名，获取页面宽度
             pwidth = (doc.pagesize[0] - 20) / 1000
+            #将页面宽度分为1000等份
             elements.append(Paragraph('Field', styles['Title']))
+            #设置表格标题
             colwidths = (pwidth*120,pwidth*120,pwidth*120,pwidth*120,pwidth*120)
-       
+            #设置表格列数及没列宽度
             table1 = Table(data, colwidths)
             table1.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
                                    ('GRID',(0,0),(-1,-1),0.5,colors.black)]))
+            #设置表格格式
             elements.append(table1)
         
         elif self.table == TABLEProductList:
@@ -104,48 +110,50 @@ class PrintTable():
             return
     
         doc.build(elements,onFirstPage=self.Pages,onLaterPages=self.Pages)
+        #生成报表
     
 
 if __name__=='__main__':
     ptable = PrintTable()
+    #实例化类
     sess = session()
     sess1 = session1()
     base.metadata.create_all (engine)
     base1.metadata.create_all (engine1)
     
-    a1 = [['BoxID','ConnectorID','Size','RisingHeight','Name']]
-    a2 = [['ItemNo','Size','Type','Name']]
-    a3 = [['BoxID','IgnitionTime','FieldID','Name']]
-    
-    with sess1.begin():
-        data1 = sess1.query(ScriptData).all()
-
-    for row1 in data1:
-        with sess1.begin():
-            row2 = sess1.query(IgnitorsData).filter_by(UUID = row1.IgnitorID).first()
-        with sess.begin():
-            row3 = sess.query(FireworksData).filter_by(UUID = row1.FireworkID).first()
-        
-        b1 = []        
-        b1.append(row2.BoxID)
-        b1.append(row3.RisingHeight)
-        b1.append(row3.Name)
-        b1.append(row3.Size)
-        b1.append(row1.ConnectorID)
-        a1.append(b1)
-        
-        b2 = []        
-        b2.append(row3.Type)
-        b2.append(row3.ItemNo)
-        b2.append(row3.Name)
-        b2.append(row3.Size)
-        a2.append(b2)
-        
-        b3 = []        
-        b3.append(row2.BoxID)
-        b3.append(row1.IgnitionTime)
-        b3.append(row3.Name)
-        b3.append(row2.FieldID)
-        a3.append(b3)
-        
-    ptable.creatTable(a1, a2, a3)
+#    a1 = [['BoxID','ConnectorID','Size','RisingHeight','Name']]
+#    a2 = [['ItemNo','Size','Type','Name']]
+#    a3 = [['BoxID','IgnitionTime','FieldID','Name']]
+#    
+#    with sess1.begin():
+#        data1 = sess1.query(ScriptData).all()
+#
+#    for row1 in data1:
+#        with sess1.begin():
+#            row2 = sess1.query(IgnitorsData).filter_by(UUID = row1.IgnitorID).first()
+#        with sess.begin():
+#            row3 = sess.query(FireworksData).filter_by(UUID = row1.FireworkID).first()
+#        
+#        b1 = []        
+#        b1.append(row2.BoxID)
+#        b1.append(row3.RisingHeight)
+#        b1.append(row3.Name)
+#        b1.append(row3.Size)
+#        b1.append(row1.ConnectorID)
+#        a1.append(b1)
+#        
+#        b2 = []        
+#        b2.append(row3.Type)
+#        b2.append(row3.ItemNo)
+#        b2.append(row3.Name)
+#        b2.append(row3.Size)
+#        a2.append(b2)
+#        
+#        b3 = []        
+#        b3.append(row2.BoxID)
+#        b3.append(row1.IgnitionTime)
+#        b3.append(row3.Name)
+#        b3.append(row2.FieldID)
+#        a3.append(b3)
+#        
+#    ptable.creatTable(a1, a2, a3)
